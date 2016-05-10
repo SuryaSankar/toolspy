67a5c8879bd3c38e596cdeec053134b35ab92d01
SuryaSankar <suryashankar.m@gmail.com>
2016-05-10 16:34:16 +0530

    changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='toolspy',
-    version='0.2.2',
+    version='0.2.3',
     description='A library of generic utility functions',
     long_description='A python toolset',
     packages=['toolspy'],
