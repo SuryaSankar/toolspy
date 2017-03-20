ee67bf6e698c788369da654eeb11584bf7e014cf
SuryaSankar <suryashankar.m@gmail.com>
2017-03-20 20:03:14 +0530

    bumping  up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='toolspy',
-    version='0.2.14',
+    version='0.2.15',
     description='A library of generic utility functions',
     long_description='A python toolset',
     packages=['toolspy'],
