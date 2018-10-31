f6d1cde7abddd3edde0b317e03a844dbd9364f91
Surya Sankar <suryashankar.m@gmail.com>
2018-11-01 01:12:41 +0530

    adding some test

---
+++ tests/test_string_tools.py
@@ -0,0 +1,16 @@
+import pytest
+
+from toolspy import string_tools
+
+
+def test_npartition_simple_case():
+    test_str = "Hello world 1234"
+    assert string_tools.npartition(
+        test_str, n=2) == ("Hello world", " ", "1234")
+
+
+def test_npartition_with_different_delim():
+    test_str = "Hello|World 123|New |Line"
+    assert string_tools.npartition(test_str, n=3, delimiter="|") == (
+        "Hello|World 123|New ", "|", "Line"
+    )
