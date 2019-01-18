12ed56e7141f3268c81c5a4fd3b9f947c75cff88
Surya Sankar <suryashankar.m@gmail.com>
2019-01-18 17:25:41 +0530

    adding datetime functions

--- toolspy/datetime_tools.py
+++ toolspy/datetime_tools.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import calendar
-
+import time
 
 def unix_time(dt):
     epoch = datetime.utcfromtimestamp(0)
@@ -12,8 +12,14 @@ def unix_time_millis(dt):
     return int(unix_time(dt) * 1000)
 
 
+def local_time_now(timedelta_mins_from_utc=None):
+    if timedelta_mins_from_utc is None:
+        timedelta_mins_from_utc = int(-time.timezone / 60)
+    return datetime.utcnow() + timedelta(minutes=timedelta_mins_from_utc)
+
+
 def ist_now():
-    return datetime.utcnow() + timedelta(minutes=330)
+    return local_time_now(timedelta_mins_from_utc=330)
 
 
 def readable_date(dt):
@@ -26,3 +32,30 @@ def totimestamp(dt, epoch=datetime(1970, 1, 1)):
     return (
         td.microseconds + (
             td.seconds + td.days * 86400) * 10**6) / 10**6
+
+
+def timedelta_mins(dt1, dt2):
+    return int((dt1 - dt2).total_seconds()/60)
+
+
+def daysdiff(d1, d2):
+    return (d1-d2).days
+
+
+def today(timedelta_mins_from_utc=None):
+    return local_time_now(timedelta_mins_from_utc=None).date()
+
+
+def n_days_ago(n, timedelta_mins_from_utc=None):
+    return today(timedelta_mins_from_utc=timedelta_mins_from_utc) - timedelta(days=n)
+
+def n_days_later(n, timedelta_mins_from_utc=None):
+    return today(timedelta_mins_from_utc=timedelta_mins_from_utc) + timedelta(days=n)
+
+
+def yesterday(timedelta_mins_from_utc=None):
+    return n_days_ago(1, timedelta_mins_from_utc=timedelta_mins_from_utc)
+
+
+def tomorrow(timedelta_mins_from_utc=None):
+    return n_days_later(1, timedelta_mins_from_utc=timedelta_mins_from_utc)
