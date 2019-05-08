a63aab9250262f7853e815a697f1e799a8534dd7
Surya Sankar <suryashankar.m@gmail.com>
2019-05-08 20:01:24 +0530

    bumping version

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SuryaSankar/toolspy',
-    version='0.2.44',
+    version='0.2.45',
     zip_safe=False,
 )
