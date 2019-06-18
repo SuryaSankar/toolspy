fc9233c43e83d06d6d8005df7f49f49d6cb3c761
SuryaSankar <suryashankar.m@gmail.com>
2019-06-18 17:55:19 +0530

    fixing a test

--- tests/test_collection_tools.py
+++ tests/test_collection_tools.py
@@ -6,4 +6,4 @@ from toolspy import collection_tools
 
 def test_unique_sublists_simple_case():
     lst = [1, 3, 5, 1, 3, 2, 6, 7, 90, 1, 7, 8]
-    assert collection_tools.unique_sublists(lst) == [[1, 3]]
+    assert collection_tools.unique_sublists(lst) ==  [[1, 3, 5, 2, 6, 7, 90, 8], [1, 3, 7], [1]]
